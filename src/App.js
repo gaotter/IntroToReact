e4a903015950d84fe9f2c8e6e26b2b495c8b8e51
gaotter <gaotter@gmail.com>
2017-06-04 20:59:57 +0200

    Hello world input to update the Hello text

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './componants/Hello';
+import InputPanel from './componants/InputPanel';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // state of an componant is the values that the componant handles.
+    this.state = {
+      helloValue: ''
+    };
+  }
+
+  updateHelloValue(value) {
+    // the state of an componant can only be replaced not changed. 
+    // set state help me make sure the state is new. Most of the time.
+    this.setState(prevState => ({ helloValue: value }));
+  }
   render() {
     return (
       <div className="App">
@@ -11,10 +25,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <div className="App-intro">
           {/* I'm a compnant. I can get data using my properties also called props. I have on propery called 'text'*/}
-          <Hello text='World' />
-        </p>
+          <Hello text={this.state.helloValue} />
+          {/* I'm a componant. I can get 'at button text' that is used as my button text, and a call back function that I
+              can send you my text value to. */}
+          <InputPanel buttonText="update hello text" valueUpdated={txt => this.updateHelloValue(txt)} />
+        </div>
       </div>
     );
   }
